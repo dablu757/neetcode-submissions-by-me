d15fd1bffef1a402622d0b18c7b8d85b8731197b
Dablu Chauhan <96309274+dablu757@users.noreply.github.com>
2026-08-20 08:51:28 +0530

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.py
@@ -0,0 +1,19 @@
+from functools import lru_cache
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+
+        n = len(nums)-1
+
+        @lru_cache(None)
+        def helper(idx):
+            if idx>=n:
+                return True
+
+            for jump in range(1,nums[idx]+1):
+                if helper(idx+jump):
+                    return True
+
+            return False
+
+        return helper(0)
+        
